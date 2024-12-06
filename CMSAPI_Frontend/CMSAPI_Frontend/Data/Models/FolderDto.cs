975a14d3f548ffda297b170f199d637db793d0ea
Oliver Sokol <oliver.sokol@nhi.no>
2024-12-06 18:35:50 +0100

    add register and document

--- CMSAPI_Frontend/CMSAPI_Frontend/Data/Models/FolderDto.cs
+++ CMSAPI_Frontend/CMSAPI_Frontend/Data/Models/FolderDto.cs
@@ -14,17 +14,6 @@ class CreateFolderDto
 
 }
 
-public class DocumentDto
-{
-    public int Id { get; set; }
-    public string? Title { get; set; }
-    public string? Content { get; set; }
-    public string? ContentType { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public string? IdentityUserId { get; set; }
-    public int? FolderId { get; set; }
-    public string? FolderName { get; set; }
-}
 
 class NavigatableFolderDto
 {
